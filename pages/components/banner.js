b2e2424cd6dba1892e6dc2914626cdc9de6bbe42
Kanna116 <muralikanna77@gmail.com>
2024-03-09 20:43:38 +0530

    products and banner made responsive

--- pages/components/banner.js
+++ pages/components/banner.js
@@ -10,11 +10,11 @@ const Banner = ({gender, category}) => {
                     src={`/images/category/banner/${gender}/${category}.webp`}
                     fill={true}
                     alt='product section hero image'
-                    className='h-full w-full object-cover '
+                    className='h-full w-full object-cover object-center'
                 />
                 <div className='w-full h-full absolute top-0 left-0  bg-gradient-to-b from-transparent to-[#00000099]'></div>
 
-                <h1 className='my-10 text-3xl  absolute z-10 bottom-10 left-20 font-semibold text-white tracking-wide capitalize'>{gender}&apos;s {gender !== category ? category : ""} Collection</h1>
+                <h1 className='my-10 text-3xl w-3/4  absolute z-10 bottom-10 lg:left-20 md:left-16 left-1/2 -translate-x-1/2 lg:-translate-x-0 md:-translate-x-0 font-semibold text-white tracking-wide capitalize'>{gender}&apos;s {gender !== category ? category : ""} Collection</h1>
             </div>
         </>
     )
--- pages/pages/[gender]/[category].js
+++ pages/pages/[gender]/[category].js
@@ -1,12 +1,11 @@
-import Image from 'next/image';
+import Banner from '@/pages/components/banner';
+import ProductContext from '@/pages/context/productContext';
 import { useRouter } from 'next/router';
 import { useContext, useState } from 'react';
 import Footer from '../../components/footer';
 import Navbar from '../../components/navbar';
-import ProductCard from '../products/productcard';
 import SortingBtn from '../../components/sortingbtn';
-import Banner from '@/pages/components/banner';
-import ProductContext from '@/pages/context/productContext';
+import ProductCard from '../products/productcard';
 
 const Category = () => {
     const router = useRouter();
@@ -54,7 +53,7 @@ const Category = () => {
 
                     <SortingBtn setSortingWay={setSortingWay} />
 
-                    <div className="product-container w-full min-h-screen grid grid-cols-4 gap-10 px-20 mt-8">
+                    <div className="product-container w-full min-h-screen grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-12  px-[24px] lg:px-[94px] md:px-[64px] place-items-center  mt-8 items-center">
                         {allCategoryProducts}
                     </div>
                 </div>
